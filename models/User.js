e37144169931e6f5a5bdd691a3135cc706519b2d
Tiffany Washke <twashke@gmail.com>
2021-06-26 23:02:45 -0700

    updated User model to include bcrypt and hooks to hash the password when created or updated

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 class User extends Model {}
 
@@ -10,26 +11,38 @@ User.init(
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
-        },
+    },
     username: {
         type: DataTypes.STRING,
         allowNull: false,
-        },
+    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
             isEmail: true,
-            },
         },
+    },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
             len: [6],
-            },
         },
+    },  
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updateUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updateUserData
+            }
+        }
     },
     {
         sequelize,
