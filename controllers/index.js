71c08ebc32840a7a9c552824d5bbfee77757ca1c
Tiffany Washke <twashke@gmail.com>
2021-06-27 16:17:00 -0700

    started adding routes

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+// const dashboardRoutes = require("./dashboardRoutes");
+
+// router.use("/dashboard", dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+
+
+module.exports = router;
