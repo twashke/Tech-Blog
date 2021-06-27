b5e66f0c9f275ee1766476ea525e49f1ad50edad
Tiffany Washke <twashke@gmail.com>
2021-06-26 22:39:48 -0700

    added logout.js file

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logout = async () => {
+    // Post Request when user logs out
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    // Return to homepage on logout
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+// Event Listener
+document.querySelector('#logout').addEventListener('click', logout);
