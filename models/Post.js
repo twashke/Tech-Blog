56dbb3ade52f0ef174f3e0c145b7f388b4ada7f1
Tiffany Washke <twashke@gmail.com>
2021-06-26 23:14:09 -0700

    updated Post.js model

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Comment extends Model {}
+class Post extends Model {}
 
-Comment.init(
+Post.init(
     {
     id: {
         type: DataTypes.INTEGER,
