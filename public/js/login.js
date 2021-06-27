9d8270e80776f1cdee9cc75d97e37dd0357ee9c4
Tiffany Washke <twashke@gmail.com>
2021-06-26 22:39:07 -0700

    created and login.js file

---
+++ public/js/login.js
@@ -0,0 +1,62 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect User Input
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    // If email and password are entered
+    if (email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        // If successful
+        if (response.ok) {
+            // redirect to the techblog page
+            document.location.replace('/techblog');
+        
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect User Input
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    // If email and password are entered
+    if (name && email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        // If successful
+        if (response.ok) {
+            // redirect to the techblog page
+            document.location.replace('/techblog');
+        } else {
+        alert(response.statusText);
+        }
+    }
+};
+
+// Event Listeners
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
