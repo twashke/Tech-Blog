0efc1a11f2fd0b806d747b55e90a73d0da0e9b04
Tiffany Washke <twashke@gmail.com>
2021-06-26 23:47:09 -0700

    added user seeds

---
+++ seeds/userSeeds.js
@@ -0,0 +1,19 @@
+const { User } = require("../models");
+
+const userData = [{
+        username: "Alice",
+        password: "alice1234"
+    },
+    {
+        username: "Alex",
+        password: "alex1234"
+    },
+    {
+        username: "Jake",
+        password: "jake1234"
+    },
+    {
+        username: "John",
+        password: "john1234"
+    },
+]
