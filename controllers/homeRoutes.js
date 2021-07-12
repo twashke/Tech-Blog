81e58bf1e71b85cebc8f8da1b86e7356f85cb462
Tiffany Washke <twashke@gmail.com>
2021-07-12 15:59:46 -0700

    updated homeRoutes to include comments and logged in feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,43 @@
 const router = require("express").Router();
+const sequelize = require("./../config/connection");
+const { Post, User, Comment } = require("./../models/");
 
 router.get("/", async (req, res) => {
     try {
-        res.render("homepage", {
+        const postData = await Post.findAll({
+            order: [["created_at", "DESC"]],
+            include: [
+                {   
+                    model: User,
+                    attributes: ["id", "username"],
+                },
+            ],
         });
+        const commentData = await Comment.findAll({
+            include: [
+                { model: User,
+                attributes: ["id", "username"]
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+        if (req.session.logged_in) {
+            res.render("homepage", { 
+                loggedIn: req.session.logged_in,
+                posts, comments,
+            });
+            console.log(comments);
+        } else {
+            res.render("homepage", {
+                loggedIn: req.session.logged_in,
+                posts,
+            });
+            console.log(posts);
+        }
     } catch {
         res.status(500).json(err);
     }
