000e2d778a50137887cb81d600fa906af632fd1d
Tiffany Washke <twashke@gmail.com>
2021-07-16 11:54:32 -0700

    updated js functions for deleting and updating comments

--- public/js/deleteComment.js
+++ public/js/deleteComment.js
@@ -1,16 +1,22 @@
 async function deleteComment(event) {
     if (event.target.hasAttribute("data-id")) {
-        const id = event.target.getAttribute("data-id");
+        // Save post_id to reload previous page
+        const post = event.target.getAttribute("data-id");
+         // Save id by taking the comment number of the window.location
+        const id = window.location.toString().split("/")[
+            window.location.toString().split("/").length - 1];
+        
         const response = await fetch(`/api/comments/${id}`, {
         method: "DELETE",
     });
         console.log(response);
     if (response.ok) {
-        document.location.replace(`/`)
+        document.location.replace(`/posts/${post}`)
     } else {
         alert("Failed to delete comment");
         }
     }
-}
+};
 
 document.querySelector("#deleteComment").addEventListener("click", deleteComment);
+
--- public/js/updateComment.js
+++ public/js/updateComment.js
@@ -1,6 +1,11 @@
 async function updateComment(event) {
     event.preventDefault();
     
+    if(event.target.hasAttribute("data-id")) {
+    // Save post_id to reload previous page
+    const post = event.target.getAttribute("data-id");
+    console.log(post);
+    // Save id by taking the comment number of the window.location
     const id = window.location.toString().split("/")[
         window.location.toString().split("/").length - 1
     ];
@@ -18,9 +23,12 @@ async function updateComment(event) {
     console.log(response);
 
     if (response.ok) {
-        document.location.replace(`/`)
+        document.location.replace(`/posts/${post}`)
     } else {
         alert("Failed to update comment");
     }
-}
+    }
+};
+
+
 document.querySelector("#updateComment").addEventListener("submit", updateComment);
